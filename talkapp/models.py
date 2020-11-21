63581d9470618fca8fc83ea0e20363a82b4008dc
Mohd Sufiyan Ansari <addsufiyan@gmail.com>
2020-11-22 00:47:01 +0530

    Form Fields Added

--- talkapp/models.py
+++ talkapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import fields
+from django.forms import ModelForm
 
 # Create your models here.
+class DataForm(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    message = models.TextField()
+    contact = models.CharField(max_length=10)
+    
+class DataIn(ModelForm):
+    class Meta:
+        model = DataForm
+        fields = ['name','message','contact']
--- talkapp/views.py
+++ talkapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import DataIn
 
 def home(request):
-	return render(request, 'talkapp/home.html')
+    form = DataIn()
+    return render(request,'talkapp/home.html',{'form':form})
